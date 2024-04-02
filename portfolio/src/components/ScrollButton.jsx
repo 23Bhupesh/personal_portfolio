a6c362ccccf85b89d2785fb9428319b0a126edc4
23Bhupesh <bhupeshsahu2312@gmail.com>
2024-04-02 18:55:53 +0530

    Footer updated

--- portfolio/src/components/ScrollButton.jsx
+++ portfolio/src/components/ScrollButton.jsx
@@ -8,7 +8,7 @@ const ScrollButton = () => {
 
   return (
     <button
-      className="fixed bottom-4 right-4 bg-purple-600 text-white px-4 py-2 rounded-full hover:bg-purple-700 focus:outline-none"
+      className="fixed bottom-4 right-4 bg-[#9B9AF3] text-white px-4 py-2 rounded-full hover:bg-[#7E69D3] "
       onClick={handleScrollToTop}
     >
       <FaArrowUp />
